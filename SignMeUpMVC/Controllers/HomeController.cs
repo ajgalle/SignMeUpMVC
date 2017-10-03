99e916a676585ac7eb22d615f0049b633269d459
Andrew <andrew.j.galle@gmail.com>
2017-10-03 04:12:56 -0400

    Last working version before experimenting with automatic entity generation experiment begins.

--- SignMeUpMVC/Controllers/HomeController.cs
+++ SignMeUpMVC/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SignMeUpMVC.Models;
 
 namespace SignMeUpMVC.Controllers
 {
@@ -10,21 +11,23 @@ namespace SignMeUpMVC.Controllers
     {
         public ActionResult Index()
         {
+            dbLocalTestEntities1 db = new dbLocalTestEntities1();
+
+            //List < tblOvertime  > list = db.tblOvertimes.ToList();
+
+            List<OvertimeViewModel> AllOvertimeList = db.tblOvertimes.Where(x => x.IsDeleted != true).Select(x => new OvertimeViewModel { DateTimeEnd = x.DateTimeEnd, DateTimeStart = x.DateTimeStart, OvertimeId = x.OvertimeId, Location = x.Location, SignUpEnd = x.SignUpEnd, SelectedEmployeeId = x.SelectedEmployeeId }).ToList();
+            
+            ViewBag.AllOvertime = AllOvertimeList;
+
+
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult AddOvertime()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
---
+++ SignMeUpMVC/Models/OvertimeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignMeUpMVC.Models
+{
+	public class OvertimeViewModel
+	{
+        public int OvertimeId { get; set; }
+        public string Location { get; set; }
+        public Nullable<System.DateTime> DateTimeStart { get; set; }
+        public Nullable<System.DateTime> DateTimeEnd { get; set; }
+        public Nullable<System.DateTime> SignUpEnd { get; set; }
+        public Nullable<int> SelectedEmployeeId { get; set; }
+        public Nullable<bool> IsDeleted { get; set; }
+
+
+    }
+}
